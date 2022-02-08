72c654db45cea1fa2e53617f98b6fd759d8d62d8
Raghu <raghunandan.yadav.18@mountblue.tech>
2022-02-08 18:51:11 +0530

    created post used bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,20 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   # get "/", to: "main#index"
   root to: "main#index"
+
+  get "sign_up", to:"registration#new"
+
+  post "sign_up", to: "registration#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "sign_in", to:"sessions#new"
+
+  post "sign_in", to: "sessions#create"
+
+  get "read_articles", to:"articles#index"
+
+  resources :articles do
+    resources :comments
+  end
 end
